9418381771ae42f91575d1a371b022b71d119994
Mark Thompson <markthompsonr@gmail.com>
2024-05-07 23:04:46 +0100

    Comment out nextconfig code test

--- next.config.js
+++ next.config.js
@@ -4,19 +4,19 @@ const nextConfig = { output: "export" };
 
 module.exports = nextConfig;
 
-module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
+// module.exports = {
+//   webpack: (config, { isServer }) => {
+//     // Fixes npm packages that depend on `fs` module
+//     if (!isServer) {
+//       config.resolve.fallback.fs = false;
+//     }
 
-    // Add markdown file handling
-    config.module.rules.push({
-      test: /\.md$/,
-      use: "raw-loader",
-    });
+//     // Add markdown file handling
+//     config.module.rules.push({
+//       test: /\.md$/,
+//       use: "raw-loader",
+//     });
 
-    return config;
-  },
-};
+//     return config;
+//   },
+// };
