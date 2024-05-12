22ab7c0ccabc0854647e14a548c05808260a01eb
Mark Thompson <markthompsonr@gmail.com>
2024-05-12 21:25:08 +0100

    Update website metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'MT',
-  description: '4th-best Mark Thompson, 2016 awards',
-}
+  title: "Mark Thompson",
+  description:
+    "Personal website of Mark Thompson. 4th-best Mark Thompson, 2016 awards.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
